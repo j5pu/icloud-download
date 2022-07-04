7f93cf408d54a4c10c6dcfc435a6a243f271de2c
root <root@example.com>
2022-07-04 15:51:19 +0200

    auto

--- api.py
+++ api.py
@@ -5,12 +5,9 @@ Icloud-download Api Module
 import os
 
 import pyicloud
-import rich
 import rich.pretty
 import rich.traceback
-
 from pyicloud import PyiCloudService
-from rich import print
 
 setattr(rich.console.Console, "is_terminal", lambda self: True)
 Console = rich.console.Console
@@ -21,14 +18,18 @@ rich.traceback.install(show_locals=True, suppress=["click", "_pytest", "rich"])
 # %load_ext autoreload
 # %autoreload 2
 
+
 def _driveservice_repr(self):
     return f"{self.__class__.__name__}({self.get_children()})"
+
 
 def _driveservice_str(self):
     return self.__repr__
 
+
 def _drive_repr(self):
     return f"{self.__class__.__name__}(name={self.name}, type={self.type})"
+
 
 def _drive_str(self):
     return self.name
@@ -43,6 +44,4 @@ pyicloud.services.drive.DriveNode.__str__ = _drive_str
 email = os.environ['EMAIL']
 password = os.environ['INTERNET']
 api = PyiCloudService(email)
-api.files.params["dsid"] = api.account.family[0].dsid # api.data['dsInfo']['dsid']
-
-
+api.files.params["dsid"] = api.account.family[0].dsid  # api.data['dsInfo']['dsid']
